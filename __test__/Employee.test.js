a0d33749a2472de0d0b7a58337c7285e1f2439e6
Terry Theis <tltheis@prodigy.net>
2021-04-21 20:29:28 -0500

    Done employee tests

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -1,7 +1,49 @@
 const Employee = require('../lib/Employee');
 const randomNumber = require('../lib/Employee');
 
-test('Employee instance', () => {
-  const e = new Employee();
-  expect(typeof(e)).toBe("object");
+test('Employee instance object', () => {
+  const runObject = new Employee();
+  expect(typeof(runObject)).toBe("object");
 });
+
+test('Name object works', () => {
+  const  name = "Ralph";
+  const runObject = new Employee(name);
+  expect(runObject.name).toBe(name);
+});
+
+test('ID object works', () => {
+  const  id = 100;
+  const runObject = new Employee("Foo", id);
+  expect(runObject.id).toBe(parseInt(id));
+});
+
+test('Email object works', () => {
+  const  email = "email@email.com";
+  const runObject = new Employee("Foo", 1, email);
+  expect(runObject.email).toBe(email);
+});
+
+test("See if getName() works", () => {
+const valTest = "RipRock";
+const name = new Employee(valTest);
+expect(name.getName()).toBe(valTest);
+});
+
+test("See if getId() works", () => {
+  const valTest = 50;
+  const name = new Employee("Foo", valTest);
+  expect(name.getId()).toBe(valTest);
+  });
+
+  test("See if getEmail() works", () => {
+    const valTest = "email@email.com";
+    const name = new Employee("Foo", 1, valTest);
+    expect(name.getEmail()).toBe(valTest);
+    });
+
+    test("See if getRole() works", () => {
+      const valTest = "Employee";
+      const role = new Employee("Alvin", 1, "email@email.com");
+      expect(role.getRole()).toBe(valTest);
+      });
