53fd6763577a39b847224e9353760279528e2982
Terry Theis <tltheis@prodigy.net>
2021-04-24 08:50:37 -0500

    test manager

--- __test__/Manger.test.js
+++ __test__/Manger.test.js
@@ -1,20 +1,20 @@
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
-test("Can set office number via constructor argument", () => {
-    const testValue = 100;
-    const e = new Manager("Foo", 1, "test@test.com", testValue);
-    expect(e.officeNumber).toBe(testValue);
+test("Office number set", () => {
+    const valTest = 10;
+    const mgr = new Manager("Foo", 1, "email@email.com", valTest);
+    expect(mgr.officeNumber).toBe(valTest);
 });
 
-test("getRole() should return \"Manager\"", () => {
-    const testValue = "Manager";
-    const e = new Manager("Foo", 1, "test@test.com", 100);
-    expect(e.getRole()).toBe(testValue);
+test("getRole() works, () => {
+    const valTest = "Manager";
+    const mgr = new Manager("Foo", 1, "email@email.com", 10);
+    expect(mgr.getRole()).toBe(valTest);
 });
 
-test("Can get office number via getOffice()", () => {
-    const testValue = 100;
-    const e = new Manager("Foo", 1, "test@test.com", testValue);
-    expect(e.getOfficeNumber()).toBe(testValue);
+test("getOffice() works", () => {
+    const valTest = 10;
+    const mgr = new Manager("Foo", 1, "email@email.com", valTest);
+    expect(mgr.getOfficeNumber()).toBe(valTest);
 });
